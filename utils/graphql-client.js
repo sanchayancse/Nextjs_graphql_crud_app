64f0bbbb50288d6c70b87f7ee0dba02327833d52
Sanchayan Das <45485694+sanchayancse@users.noreply.github.com>
2020-11-01 23:43:55 +0530

    All file added

---
+++ utils/graphql-client.js
@@ -0,0 +1,10 @@
+// utils/graphql-client.js
+import { GraphQLClient } from 'graphql-request';
+
+const endpoint = 'https://graphql.fauna.com/graphql';
+export const graphQLClient = new GraphQLClient(endpoint, {
+  headers: {
+    authorization: `Bearer ${process.env.NEXT_PUBLIC_FAUNA_SECRET}`,
+    'X-Schema-Preview': 'partial-update-mutation',
+  },
+});
